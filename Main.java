adb5dbb0abc4c66cbfbb17d660dc744bd96f78e6
JavierRamirezCospin <35494248+JavierRamirezCospin@users.noreply.github.com>
2019-02-08 12:37:55 -0600

    Main Class
    Clase Main del programa

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+
+	     Scanner teclado = new Scanner(System.in);
+		 	 int n = 0;
+
+		 	 while (n != 7) {
+		    		System.out.println("-------------------------------");
+		    		System.out.println("        MENU PRINCIPAL      ");
+		    		System.out.println("-------------------------------");
+            System.out.println("");
+            System.out.println("1. Gnome Sort");
+            System.out.println("2. Merge Sort");
+            System.out.println("3. Quick Sort");
+            System.out.println("4. Radix Sort");
+            System.out.println("5. Selection Sort");
+            System.out.println("6. Salir");
+            System.out.println("Ingrese su opcion: ");
+            n = teclado.nextInt();
+            if(n == 0) {
+                System.out.println("Ingresar un numero mayor a 0");
+            }
+            if(n == 1) {
+								Sorting.GnomeSort();
+            }
+            if(n == 2) {
+								Sorting.MergeSort();
+            }
+            if(n == 3) {
+								Sorting.QuickSort();
+            }
+            if(n == 4) {
+								Sorting.RadixSort();
+            }
+            if(n == 5) {
+								Sorting.SelectionSort();
+            }
+            if(n == 6) {
+                System.out.println("Gracias por usar la aplicacion");
+            }
+            if(n >= 7) {
+                System.out.println("Ingresar un numero menor a 7");
+                n = 0;
+            }
+		}
+	}
+}
