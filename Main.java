2dca0c193ea1a9c003b5fe53a8cc504f01a2a88b
JavierRamirezCospin <35494248+JavierRamirezCospin@users.noreply.github.com>
2019-02-08 12:41:55 -0600

    Update Class Main

--- Main.java
+++ Main.java
@@ -4,8 +4,41 @@ import java.util.*;
 class GFG {
 	public static void main (String[] args) {
 
-	     Scanner teclado = new Scanner(System.in);
-		 	 int n = 0;
+
+		Scanner teclado = new Scanner(System.in);
+		int n = 0;
+		int[] toBeSorted = new int[100];
+
+			for (int i = 0; i < 100; i++) {
+					int test = new Random().nextInt(Integer.SIZE - 1);
+					toBeSorted[i] = test;
+			}
+
+			for (int i = 1; i <= toBeSorted.length; i++) {
+					System.out.println(i + ":" + toBeSorted[i - 1]);
+			}
+
+			testClass test = new testClass();
+			testClass2 test2 = new testClass2();
+			/**
+			 int[] sorted = test.GnomeSort(toBeSorted);
+			 for(int i=1; i<=sorted.length; i++){
+			 System.out.println(i+":"+sorted[i-1]);
+			 }
+			 **/
+
+			int[] listanum = new int[10];
+			for (int i = 0; i < listanum.length; i++) {
+					listanum[i] = new Random().nextInt(50);
+			}
+
+			test2.sort(toBeSorted, 0, toBeSorted.length - 1);
+
+			for (int i = 1; i <= toBeSorted.length; i++) {
+					System.out.println(i + ":" + toBeSorted[i - 1]);
+			}
+
+
 
 		 	 while (n != 7) {
 		    		System.out.println("-------------------------------");
